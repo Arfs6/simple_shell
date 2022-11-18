c09b8b970c5abe819c1da187cc019b477014b1c3
Abdulqadir Ahmad <2004a3abuahmad@gmail.com>
2022-11-18 21:09:31 +0100

    main.c

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	while (TRUE)
 	{
 		if (argc == 1 && isatty(STDIN_FILENO) == 1)
-			_puts(STDOUT_FILENO, "($) ");
+			_puts(STDOUT_FILENO, "$ ");
 		lineNo++;
 		vector = getCmd(&path, &status, argv[0], lineNo);
 		if (vector == NULL)
